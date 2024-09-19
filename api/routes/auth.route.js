6bebaa9632f9ff34d6b4915f385dcd49bc552a48
Sadia <sadiajabeen0607@gmail.com>
2024-09-20 00:30:27 +0500

    Create signup api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signUp } from '../controllers/auth.controller.js';
+import { signin, signUp } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.post('/signup', signUp)
+router.post('/signup', signUp);
+router.post('/signin', signin);
 
 export default router;
