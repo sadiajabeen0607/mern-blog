bccfdec379b380917e14bac894a3cf7e2caa71af
Sadia <sadiajabeen0607@gmail.com>
2024-11-01 22:42:39 +0500

    Showing comments of a posts

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,73 +1,135 @@
-import { Alert, Button, Textarea } from 'flowbite-react';
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Alert, Button, Textarea } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Comment from "./Comment";
 
-export default function CommentSection({postId}) {
+export default function CommentSection({ postId }) {
   // console.log('postId', postId);
 
   const { currentUser } = useSelector((state) => state.user);
 
-  const [comment, setComment] = useState('');
+  const [comment, setComment] = useState("");
   const [commentError, setCommentError] = useState(null);
+  const [comments, setComments] = useState([]);
+  // console.log(comments);
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const res = await fetch('/api/comment/create', {
-        method: 'POST',
+      const res = await fetch("/api/comment/create", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({content: comment, postId, userId: currentUser._id})
+        body: JSON.stringify({
+          content: comment,
+          postId,  
+          userId: currentUser._id,
+        }),
       });
-  
+
       const data = await res.json();
-  
-      if(res.ok) {
-        setComment('');
+
+      if (res.ok) {
+        setComment("");
         setCommentError(null);
+        setComments([data, ...comments])
       }
-      
     } catch (error) {
       setCommentError(error.message);
     }
+  };
 
-  }
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const res = await fetch(`/api/comment/getPostComments/${postId}`);
+        // console.log("responce", res);
+
+        if (res.ok) {
+          const data = await res.json();
+          setComments(data);
+          // console.log("data", data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getComments();
+  }, [postId]);
 
   return (
-    <div className='max-w-2xl mx-auto p-3 w-full'>
+    <div className="max-w-2xl mx-auto p-3 w-full">
       {currentUser ? (
-        <div className='flex items-center gap-1 text-gray-500 text-sm my-5'>
+        <div className="flex items-center gap-1 text-gray-500 text-sm my-5">
           <p>Signed in as:</p>
-          <img className='h-5 w-5 rounded-full object-cover' src={currentUser.profilePicture} alt={currentUser.username} />
-          <Link to={'/dashboard?tab=profile'} className='text-cyan-500 hover:underline text-xs'>
+          <img
+            className="h-5 w-5 rounded-full object-cover"
+            src={currentUser.profilePicture}
+            alt={currentUser.username}
+          />
+          <Link
+            to={"/dashboard?tab=profile"}
+            className="text-cyan-500 hover:underline text-xs"
+          >
             @{currentUser.username}
           </Link>
         </div>
       ) : (
-        <div className='text-sm text-teal-500 my-5 flex gap-1'>
+        <div className="text-sm text-teal-500 my-5 flex gap-1">
           You must be signed in to comment.
-          <Link className='text-blue-500 hover:underline' to={'/sign-in'}>
+          <Link className="text-blue-500 hover:underline" to={"/sign-in"}>
             Sign In
           </Link>
         </div>
       )}
 
       {currentUser && (
-        <form onSubmit={handleSubmit} className='border border-teal-500 rounded-md p-3'>
-          <Textarea rows='3' maxLength='200' placeholder='Add a comment...' onChange={(e) => setComment(e.target.value)} value={comment} />
-          <div className='flex items-center justify-between mt-5'>
-            <p className='text-gray-500 text-xs'>{200 - comment.length} characters remaining</p>
-            <Button outline gradientDuoTone='purpleToBlue' type='submit'>Submit</Button>
+        <form
+          onSubmit={handleSubmit}
+          className="border border-teal-500 rounded-md p-3"
+        >
+          <Textarea
+            rows="3"
+            maxLength="200"
+            placeholder="Add a comment..."
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
+          />
+          <div className="flex items-center justify-between mt-5">
+            <p className="text-gray-500 text-xs">
+              {200 - comment.length} characters remaining
+            </p>
+            <Button outline gradientDuoTone="purpleToBlue" type="submit">
+              Submit
+            </Button>
           </div>
           {commentError && (
-            <Alert color='failure' className='mt-5'>{commentError}</Alert>
-          )} 
+            <Alert color="failure" className="mt-5">
+              {commentError}
+            </Alert>
+          )}
         </form>
       )}
+      {comments.length === 0 ? (
+        <p className="text-sm my-5">No Comments yet</p>
+      ) : (
+        <>
+        <div className="flex items-center gap-1 text-sm my-5">
+          <p>Comments</p>
+          <span className="border border-gray-400 py-1 px-2 rounded-sm">
+            {comments.length}
+          </span>
+        </div>
+        {comments.map((comment) =>(
+          <Comment key={comment._id}
+          comment={comment} />
+        ))}
+        </>
+      )}
     </div>
-  )
+  );
 }
- 
